7f728e5d3629b98814dab73cc6deecc18828cefe
Douglas Rezende <douglas.dsrrezende@gmail.com>
2025-09-18 21:29:14 -0300

    Primeiros arquivos e java

---
+++ soma.java
@@ -0,0 +1,9 @@
+public class soma {
+    public static void main(String[]args){
+        int a=5;
+        int b=10;
+        int soma = a+b;
+
+        System.out.println("A soma de a "+ a + " + " + b + " é: " + soma);
+    }
+}
